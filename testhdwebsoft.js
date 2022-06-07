cb60fdb04019fd99e91f6c9d28fe8af2e8c36916
DT0998 <duongkytrieuvn@gmail.com>
2022-06-07 15:09:09 +0700

    update test pv

--- testhdwebsoft.js
+++ testhdwebsoft.js
@@ -6,7 +6,6 @@
 "usememo";
 "usecallback";
 
-
 // reduce array to obj
 // const posts = [
 //   {
@@ -34,30 +33,40 @@
 // let newPosts = posts.reduce(reducer,{})
 // console.log(newPosts);
 
-// recursion integer
-// function rangeOfNumber(startNum,endNum) {
-//    return []
-// }
-// console.log();
+// recursion prime number
+function PrimeNumber(num) {
+  if (num === 2) return true;
+  if (num < 2 || num % 2 === 0) return false;
+  for (let i = 3; i * i <= num; i += 2) {
+    if (num % i === 0) return false;
+  }
+  return true;
+}
+
+console.log(PrimeNumber(6));
 
 // let vs var
 // var
-function testVarLoop(){
-    for (var i = 0; i < 5; i++) {
-     console.log(i);
-    }
-    console.log("outside i",i);
-};
-testVarLoop()
+// function testVarLoop(){
+//     for (var i = 0; i < 5; i++) {
+//      console.log(i);
+//     }
+// khác nhau khi mỗi lần lập là lập cùng biến giá trị
+// console.log("outside i",i);
+// };
+// testVarLoop()
 // let
-function testLetLoop(){
-    for (let i = 0; i < 5; i++) {
-     console.log(i);
-    }
-    // global throw error
-    // console.log("outside i",i);
-};
-testLetLoop()
+// function testLetLoop(){
+//     for (let i = 0; i < 5; i++) {
+//      console.log(i);
+//     }
+// global throw error function scope
+// khác nhau khi mỗi lần lập là tạo ra biến giá trị mới
+// console.log("outside i",i);
+// };
+// testLetLoop()
 
-
-
+// hoisting
+// var a = 10
+// var a = 20
+// console.log(a);
