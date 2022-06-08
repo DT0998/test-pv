b437d2544a99ebf2284c12ad634f5ccf6eeada0c
DT0998 <duongkytrieuvn@gmail.com>
2022-06-08 19:09:12 +0700

    update reduce age object array

--- test-pv-js-vanilla/testpv.js
+++ test-pv-js-vanilla/testpv.js
@@ -17,20 +17,37 @@
 // remove duplicate string
 // const str = "Coooovvvidd 19"
 
-
 // tìm số lớn thứ 2 vs vòng for
-const array = [1,4,5,6,3];
-let largestNum = array[0];
-let secondLargestNum = 0;
-for(let i = 0; i < array.length;i++){
-    if(array[i] > largestNum){
-        secondLargestNum = largestNum;
-        largestNum = array[i]
-    }
-    if(array[i] !== largestNum && array[i] > secondLargestNum){
-        secondLargestNum = array[i]
-    }
-};
+// const array = [1,4,5,6,3];
+// let largestNum = array[0];
+// let secondLargestNum = 0;
+// for(let i = 0; i < array.length;i++){
+//     if(array[i] > largestNum){
+//         secondLargestNum = largestNum;
+//         largestNum = array[i]
+//     }
+//     if(array[i] !== largestNum && array[i] > secondLargestNum){
+//         secondLargestNum = array[i]
+//     }
+// };
 
-console.log(secondLargestNum);
+// console.log(secondLargestNum);
 
+// sum array object
+const age = [
+  {
+    age: 10,
+  },
+  {
+    age: 20,
+  },
+  {
+    age: 30,
+  },
+];
+const arr = Object.values(age);
+console.log(arr);
+const SumAge = arr.reduce((previousAge, currentAge) => {
+  return previousAge + currentAge.age;
+}, 0);
+console.log(SumAge);
