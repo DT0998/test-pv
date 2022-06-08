8b6009d7b0d4261813d0688623277c0c7e691472
DT0998 <duongkytrieuvn@gmail.com>
2022-06-08 16:00:49 +0700

    update second largest number

--- test-pv-js-vanilla/testpv.js
+++ test-pv-js-vanilla/testpv.js
@@ -14,3 +14,23 @@
 
 // console.log("1" - 1);
 
+// remove duplicate string
+// const str = "Coooovvvidd 19"
+
+
+// tìm số lớn thứ 2 vs vòng for
+const array = [1,4,5,6,3];
+let largestNum = array[0];
+let secondLargestNum = 0;
+for(let i = 0; i < array.length;i++){
+    if(array[i] > largestNum){
+        secondLargestNum = largestNum;
+        largestNum = array[i]
+    }
+    if(array[i] !== largestNum && array[i] > secondLargestNum){
+        secondLargestNum = array[i]
+    }
+};
+
+console.log(secondLargestNum);
+
