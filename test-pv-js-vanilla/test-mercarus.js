a42f2165c268ea8907631d91f040bc4dcf8c9237
DT0998 <duongkytrieuvn@gmail.com>
2024-02-07 20:58:16 +0700

    feat: add test

--- test-pv-js-vanilla/test-mercarus.js
+++ test-pv-js-vanilla/test-mercarus.js
@@ -1,26 +1,26 @@
 // Given an array of integers. Every numbers appear twice except one. Write a function to find that number?
 
-const arr = [2, 2, 1, 3, 3];
+const arr = [1, 1, 3, 3, 4, 2, 4];
 function findSingleNumber(arr) {
-    let result = 0;
-    for (let i = 0; i < arr.length; i++) {
-        result ^= arr[i];
-    }
-    return result;
+  let result = 0;
+  for (let i = 0; i < arr.length; i++) {
+    result ^= arr[i];
+  }
+  return result;
 }
 console.log(findSingleNumber(arr));
 
 // Write a function that takes a string and checks if any word in it has duplicate letters.
-const str = 'hello world';
+const str = "hello world";
 
 function hasDuplicateLetters(str) {
-    const words = str.split(' ');
-    for (const word of words) {
-        const wordSet = new Set(word);
-        if(word.length !== wordSet.size) {
-            return true;
-        }
+  const words = str.split(" ");
+  for (const word of words) {
+    const wordSet = new Set(word);
+    if (word.length !== wordSet.size) {
+      return true;
     }
-    return false 
+  }
+  return false;
 }
 console.log(hasDuplicateLetters(str));
