3cb4c89650e428f5ab552ccc265040de1a2cde5f
DT0998 <duongkytrieuvn@gmail.com>
2024-03-03 15:51:26 +0700

    fix: update correct prime

--- test-pv-js-vanilla/test.js
+++ test-pv-js-vanilla/test.js
@@ -11,15 +11,25 @@
 // myAsync()
 
 // số nguyên tố
-// const isPrime = (num) => {
-//  for(let i = 2;i<num;i++){
-//   if(num % i === 0){
-//     return false
-//   }
-//   return num > 1
-//  }
-// };
-// console.log(isPrime(10));
+const isPrime = (num) => {
+    if (num === 2) {
+      return true;
+    }
+  
+    if (num <= 1) {
+      return false;
+    }
+  
+    for (let i = 2; i < num; i++) {
+      if (num % i === 0) {
+        return false;
+      }
+    }
+  
+    return true;
+  };
+isPrime(6);
+// console.log(isPrime(5));
 
 
 // findallincrease must fix
